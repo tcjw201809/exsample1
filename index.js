fbcf84adc0f9fadcec5fdd133219ac692fb6a58d
千葉 健司 <t_chiba@joyworks.jp>
2020-09-16 14:25:40 +0900

    jsonをレスポンスで返すAPIを追加

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const app = express();
 
 const portNo = 3000;
 // Routing
-app.get('/', (req, res) => res.send('Hello Test'));
+
+// localhost:3000/api/v1/listにGETをかけた場合のレスポンス
+app.get('/api/v1/list', (req, res) => {
+    // 送信データ
+    const todoList = [
+        {title: 'todo1', done: true},
+        {title: 'todo2', done: false},
+        {title: 'todo3', done: false},
+    ];
+
+    // データ送信
+    res.json(todoList);
+})
+
+//app.get('/', (req, res) => res.send('Hello Test'));
 
 // 指定したポート番号でサーバー実行
 app.listen(portNo, () => {console.log(`Listening on port ${portNo}`)})
